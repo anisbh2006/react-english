d0cbb40ae8274460812955a46cc30c5f53e8cc47
anis <anis252216@email.cmm>
2024-12-04 16:18:12 +0100

    aa

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  letO
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3'>Vite with taiwind</h1>
+      <Card username="hitesh"/>
+      <Card username='Json'post='Staff Engg.'/>
+      <Card />
+    </>
+  )
+}
+
+export default App
