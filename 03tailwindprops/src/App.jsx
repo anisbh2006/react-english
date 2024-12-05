a1bb9f05b5dd9e92611879bee22a0ab5ec7b7254
anis <anis252216@email.cmm>
2024-12-05 10:45:34 +0100

    from video

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,11 +6,21 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
-  letO
+  let Obj = {
+    name: 'anis',
+    age: 18,
+    address: {
+      city: 'kolea',
+      state:'tipaza',
+      country: 'Algeria'
+    }
+  }
+  let newArr = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ]
+
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3'>Vite with taiwind</h1>
-      <Card username="hitesh"/>
+      <Card username="hitesh" myArr = {newArr}/>
       <Card username='Json'post='Staff Engg.'/>
       <Card />
     </>
