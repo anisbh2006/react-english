ba99f0ce664e2f2583be2339277e8cc51b470807
anis <anis252216@email.cmm>
2024-12-10 18:02:55 +0100

    11

---
+++ 11blogapp/src/conf/conf.js
@@ -0,0 +1,13 @@
+const conf = {
+    appwriteUrl: String(prpcess.env.
+     REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.
+    REACT_APP_APPWRITE_PROJECTID_ID),
+    appwriteDatabaseId: String(process.env.
+    REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(process.env.
+    REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.
+    REACT_APP_APPWRITE_BUCKET_ID)
+}
+export default conf
